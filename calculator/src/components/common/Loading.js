312d8065c4954bf616a13f706be8222986623042
enovz <zvonimir.bioci.email@gmail.com>
2018-01-24 17:47:32 +0100

    update

--- calculator/src/components/common/Loading.js
+++ calculator/src/components/common/Loading.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import "../../styles/Loading.css";
 
 class Loading extends Component {
-  constructor(props) {
-    super(props);
-  }
-  //{
   render() {
     return (
       <div className="spinner" style={{ display: this.props.display }}>
